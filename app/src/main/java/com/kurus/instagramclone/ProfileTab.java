12c9fc4ccfb9c225d66b897248f4dcdf47afa08b
ho2ri2s <ho2ri2s.ou@gmail.com>
2019-05-04 15:14:17 +0900

    Implemented the users tab

--- app/src/main/java/com/kurus/instagramclone/ProfileTab.java
+++ app/src/main/java/com/kurus/instagramclone/ProfileTab.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,6 +21,9 @@ import androidx.fragment.app.Fragment;
  */
 public class ProfileTab extends Fragment {
 
+    private EditText edtProfileName, edtProfileBio, edtProfileProfession,
+            edtProfileHobbies, edtProfileFavSport;
+    private Button btnUpdateInfo;
 
     public ProfileTab() {
         // Required empty public constructor
@@ -24,7 +34,57 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        edtProfileName = view.findViewById(R.id.edtProfileName);
+        edtProfileBio = view.findViewById(R.id.edtProfileBio);
+        edtProfileProfession = view.findViewById(R.id.edtProfileProfession);
+        edtProfileHobbies = view.findViewById(R.id.edtProfileHobbies);
+        edtProfileFavSport = view.findViewById(R.id.edtProfileFavSport);
+
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateInfo);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        if(parseUser.get("profileName") == null){
+            edtProfileName.setText("");
+        }else{
+            edtProfileName.setText(parseUser.get("profileName").toString());
+        }
+
+        //if a profileName of the parseUser is null, this App will crush.
+        //So, not add toString but "".
+        edtProfileBio.setText(parseUser.get("profileBio") + "");
+        edtProfileProfession.setText(parseUser.get("profileProfession") + "");
+        edtProfileHobbies.setText(parseUser.get("profileHobbies") + "");
+        edtProfileFavSport.setText(parseUser.get("profileFavSport") + "");
+
+
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parseUser.put("profileName", edtProfileName.getText().toString());
+                parseUser.put("profileBio", edtProfileBio.getText().toString());
+                parseUser.put("profileProfession", edtProfileProfession.getText().toString());
+                parseUser.put("profileHobbies", edtProfileHobbies.getText().toString());
+                parseUser.put("profileFavSport", edtProfileFavSport.getText().toString());
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            Toast.makeText(getContext(), "Info Updated", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+        return view;
     }
 
 }
